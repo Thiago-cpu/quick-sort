0150d1f9ec30ecd5f41e759d10a5ac2490a10838
thiago <thiago@dreamcode.io>
2023-12-28 13:42:03 -0300

    add theme switch

---
+++ src/components/theme-switch.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useTheme } from "next-themes";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+
+export default function ThemeSwitch() {
+  const { setTheme, resolvedTheme } = useTheme();
+  const toggleTheme = () => {
+    if (resolvedTheme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={toggleTheme}
+      className="absolute top-4 right-4"
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+    </Button>
+  );
+}
