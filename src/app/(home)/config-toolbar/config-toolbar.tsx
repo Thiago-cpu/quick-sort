75ca0f0bda0ffa09da4c64825756efd5d3dabe18
thiago <thiago@dreamcode.io>
2023-12-29 10:42:33 -0300

    add config toolbar

---
+++ src/app/(home)/config-toolbar/config-toolbar.tsx
@@ -0,0 +1,12 @@
+import { Slider } from "@/components/ui/slider";
+import SeedRandomizer from "./seed-randomizer";
+import ElementsAmount from "./elements-amount";
+
+export default function ConfigToolbar() {
+  return (
+    <div className="flex flex-row gap-4">
+      <ElementsAmount />
+      <SeedRandomizer />
+    </div>
+  );
+}
